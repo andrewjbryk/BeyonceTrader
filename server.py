b8d418b70d10dbdeb962e3c4e8667ff9006292a5
Abhi Agarwal <me@abhiagarwal.com>
2014-02-15 00:14:50 -0500

    Adding server, api

--- server.py
+++ server.py
@@ -2,22 +2,43 @@ import tornado.ioloop
 import tornado.web
 from tornado.web import StaticFileHandler
 from tornado.options import define, options
-import json
+import json, ystockquote, unicodedata
 
 define("port", default=8888, help="run on the given port", type=int)
 
+def normalize(param1):
+    query = unicodedata.normalize('NFKD', param1).encode('ascii','ignore')
+    splitArray = query.split()
+    query = " ".join(splitArray)
+    return query
+
 class RootHandler(tornado.web.RequestHandler):
-    def get(self):
+    def get(self, param1):
         self.render('index.html')
+
+class APIHandler(tornado.web.RequestHandler):
+    def get(self, param1):
+        query = normalize(param1)
+        endResult = ystockquote.get_all(query)
+        if endResult["volume"] is not "0":
+            self.write(tornado.escape.json_encode(endResult))
+        else:
+            self.write(tornado.escape.json_encode({"Result": "N/A"}))
+
+class HomepageHandler(tornado.web.RequestHandler):
+    def get(self):
+        endResult = ystockquote.get_all('^IXIC')
+        self.write(tornado.escape.json_encode(endResult))
 
 def main():
     tornado.options.parse_command_line()
     app = tornado.web.Application([
-        (r"/", RootHandler),
-        (r"/(.+)", StaticFileHandler, dict(path='lib')),
+        (r"/(?P<param1>[^\/]+)?", RootHandler),
+        (r"/s/homepage.json", HomepageHandler),
+        (r"/api/(?P<param1>[^\/]+)?.json", APIHandler),
+        (r"/lib/(.+)", StaticFileHandler, dict(path='lib')),
     ])
     app.listen(options.port)
-
     print "Application ready and listening @ %i" % (options.port)
     tornado.ioloop.IOLoop.instance().start()
 
